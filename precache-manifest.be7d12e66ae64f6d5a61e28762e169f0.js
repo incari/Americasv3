65c4c4ef111b72d69640b3ee2b91766eb10d1fed
Martin Racana <ing.racana@gmail.com>
2020-04-15 22:42:03 +0200

    Updates

---
+++ precache-manifest.be7d12e66ae64f6d5a61e28762e169f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3caea875255653463cbc45c489ce6c2",
+    "url": "/Americasv3/index.html"
+  },
+  {
+    "revision": "0f805430d678feaa3e64",
+    "url": "/Americasv3/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c87c773fd60a52b67a82",
+    "url": "/Americasv3/static/css/main.e57fccfb.chunk.css"
+  },
+  {
+    "revision": "0f805430d678feaa3e64",
+    "url": "/Americasv3/static/js/2.e17acf55.chunk.js"
+  },
+  {
+    "revision": "2c85171c75e1ffc85726f1b3d0a0b707",
+    "url": "/Americasv3/static/js/2.e17acf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c87c773fd60a52b67a82",
+    "url": "/Americasv3/static/js/main.81351f7f.chunk.js"
+  },
+  {
+    "revision": "d80f1ccae0b16366b34a",
+    "url": "/Americasv3/static/js/runtime-main.1fd078ad.js"
+  },
+  {
+    "revision": "37f73bb36cf4742486ddc3b55d67ebf4",
+    "url": "/Americasv3/static/media/Americas-logo.37f73bb3.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Americasv3/static/media/logo.5d5d9eef.svg"
+  }
+]);
